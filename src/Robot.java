9ca4551395a507e0c00eab57b77f44c5af125420
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-01-24 09:25:08 +0300

    print вместо void - int , + return. Запись переменной this.x = print(this.x, x, "x");                                                               this.y = print(this.y, y, "y");

--- src/Robot.java
+++ src/Robot.java
@@ -1,16 +1,18 @@
 
-    public class Robot {
-        int x;
-        int y;
+public class Robot {
+    int x;
+    int y;
 
-        public void move(int x, int y) {
-            print(this.x, x, "x");
-            print(this.y, y, "y");
-        }
-        public void print(int start, int end, String axisName){
-            while(start < end){
-                System.out.println("Двигаемся по оси " + axisName + " от " + start + " до " + (++start));
-            }
-        }
+    public void move(int x, int y) {
+        this.x = print(this.x, x, "x");
+        this.y = print(this.y, y, "y");
     }
+
+    public int print(int start, int end, String axisName) {
+        while (start < end) {
+            System.out.println("Двигаемся по оси " + axisName + " от " + start + " до " + (++start));
+        }
+        return start;
+    }
+}
 
